31d8fbcdaac00b45627e4069e5d818355df4e743
AntAbramov <ant.itdev@icloud.com>
2023-02-15 17:24:35 +0500

    add a project

---
+++ CardViewSUI/CardViewSUI/CardViewSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewSUIApp.swift
+//  CardViewSUI
+//
+//  Created by Anton Abramov on 15.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardViewSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
