4955bff0d02672d5244b405e22aecd1563023b87
AntAbramov <ant.itdev@icloud.com>
2023-02-15 21:03:34 +0500

    add custom view

--- CardViewSUI/CardViewSUI/Person.swift
+++ CardViewSUI/CardViewSUI/Person.swift
@@ -5,3 +5,8 @@ struct Person {
     let followerCount: Int
     let jobTitle: String
 }
+
+let person1 = Person(headerImage: "designerHeader", profileImage: "designerProfile",
+                     name: "Lara Brown", followerCount: 2384, jobTitle: "Designer")
+let person2 = Person(headerImage: "developerHeader", profileImage: "developerProfile",
+                     name: "Jack Davis", followerCount: 1840, jobTitle: "Developer")
--- CardViewSUI/CardViewSUI/PersonCardView.swift
+++ CardViewSUI/CardViewSUI/PersonCardView.swift
@@ -1,13 +1,74 @@
 import SwiftUI
 
 struct PersonCardView: View {
+    let person: Person
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Image(person.headerImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 80)
+                    .clipped()
+                
+                Image(person.profileImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 80)
+                    .clipShape(Circle())
+                    .offset(y: 40)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+            }
+            .overlay(alignment: .topTrailing) {
+                Button {
+                    //TODO: action
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                        .padding(3)
+                }
+            }
+            
+            VStack(spacing: 0) {
+                Button {
+                    //TODO: action
+                } label: {
+                    Text("Follow")
+                        .padding(.vertical, 4)
+                        .padding(.horizontal)
+                        .overlay {
+                            Capsule()
+                                .stroke(lineWidth: 2)
+                        }
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding()
+                
+                HStack {
+                    Text(person.name)
+                        .fontWeight(.bold)
+                    Text("· \(person.followerCount) Followers")
+                        .foregroundColor(.secondary)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+                
+                Text(person.jobTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding([.leading, .bottom])
+            }
+        }
+        .background(Color(.tertiarySystemFill))
+        .cornerRadius(12)
     }
 }
 
 struct PersonCardView_Previews: PreviewProvider {
     static var previews: some View {
-        PersonCardView()
+        PersonCardView(person: person1)
+            .previewLayout(.sizeThatFits)
     }
 }
